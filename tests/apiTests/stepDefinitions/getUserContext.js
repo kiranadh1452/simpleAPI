7a9e682c9f6c657479f5f0c63807912a8d282471
Kiran Adhikari <kiranadh1452@gmail.com>
2022-07-05 08:56:10 +0545

    Test progress: Response fetched

--- tests/apiTests/stepDefinitions/getUserContext.js
+++ tests/apiTests/stepDefinitions/getUserContext.js
@@ -1,26 +1,46 @@
 const assert = require("assert");
 const { When, Then } = require("@cucumber/cucumber");
 
-When('the user makes request for all users data', function () {
-  // Write code here that turns the phrase above into concrete actions
+let response = null;
+
+When("the user makes request for all users data", async function () {
+    response = await GET("/api/users");
 });
 
-When('the user makes request for a single user data with an id {string}', function (idValue) {
-  // Write code here that turns the phrase above into concrete actions
-});
+When(
+    "the user makes request for a single user data with an id {string}",
+    async function (idValue) {
+        response = await GET(`/api/user/id=${idValue}`);
+    }
+);
 
-When('the user makes request for a single user data with a name {string}', function (nameValue) {
-  // Write code here that turns the phrase above into concrete actions
-});
+When(
+    "the user makes request for a single user data with a name {string}",
+    async function (nameValue) {
+        response = await GET(`/api/user/name=${nameValue}`);
+    }
+);
 
-Then('the user should get a response code of {string}', function (responseCode) {
-  // Write code here that turns the phrase above into concrete actions
-});
+Then(
+    "the user should get a response code of {string}",
+    async function (responseCode) {
+        const statusCode = await response.status;
 
-Then('the user should see {string} and {string} in the response', function (param1, param2) {
-  // Write code here that turns the phrase above into concrete actions
-});
+        assert.equal(
+            statusCode,
+            responseCode,
+            `Expected status code ${responseCode} but got ${statusCode}`
+        );
+    }
+);
 
-Then('the user should get a single entry in the response', function () {
-  // Write code here that turns the phrase above into concrete actions
+Then(
+    "the user should see {string} and {string} in the response",
+    function (param1, param2) {
+        // code here to check if the response has param1 and param2
+    }
+);
+
+Then("the user should get a single entry in the response", async function () {
+    // code here to check that response has only one object
 });
